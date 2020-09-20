e8bc95d63a93614195cc13efa7254862d1da9b61
Ardian Adam Alfarisyi <aadama@MacBook-Pro-Ardian.local>
2020-09-20 19:40:51 +0700

    HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Ardian Adam Alfarisyi");
+		System.out.println("NIM : 6706204067");
+		System.out.println("Kelas : D3IF-44-03");
+	}
+}
